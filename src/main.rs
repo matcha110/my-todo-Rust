08c85b4c3620c208a7269a5fc1db2f993666f58e
matcha110 <121884964+matcha110@users.noreply.github.com>
2023-11-25 19:20:13 +0900

    add section3-5

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use axum::{
     routing::{get, post},
     Router,
 };
-use handlers::create_todo;
+use handlers::{all_todo, create_todo, delete_todo, find_todo, update_todo};
 use std::net::SocketAddr;
 use std:: {
     env,
@@ -36,7 +36,11 @@ async fn main() {
 fn create_app<T: TodoRepository>(repository: T) -> Router {
     Router::new()
         .route("/", get(root))
-        .route("/todos", post(create_todo::<T>))
+        .route("/todos", post(create_todo::<T>).get(all_todo::<T>))
+        .route("/todos/:id", get(find_todo::<T>)
+            .delete(delete_todo::<T>)
+            .patch(update_todo::<T>),
+        )
         .layer(Extension(Arc::new(repository)))
 }
 
